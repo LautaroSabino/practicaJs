43131dad51417dc1e04924d8af327e87bdd20d95
Lautaro Sabino <lautarosabino26@gmail.com>
2023-08-08 11:41:54 -0300

    iniciando variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// esta es la forma de comentar una sola línea
+
+/* esta es la forma de comentar en dos lineas
+ves? son dos lineas o mas */
+
+let nombre = "lau";
+const apellido = "sabino";
+let edad = 34;
+let valor = true;
