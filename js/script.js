fe0ec8aac62a03f0a2aac106c9eec0fef998bf2b
Lautaro Sabino <lautarosabino26@gmail.com>
2023-08-08 13:29:27 -0300

    jugando con prompt

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@
 /* esta es la forma de comentar en dos lineas
 ves? son dos lineas o mas */
 
-let nombre = "lau";
-const apellido = "sabino";
-let edad = 34;
-let valor = true;
+/* let userName = "Lautaro";
+const userLastName = "Sabino";
+let age = 34;
+let value = true;
+let quote = 'esta es una cadena de caracteres'
+
+let numA = 25;
+let numB = 35;
+let resultado = numA % numB;
+
+let userNameComplete = userName + " " + userLastName; */
+
+let userName = prompt('ingresá tu nombre:');
+let userLastName = prompt('ingrese su apellido:');
+let userAge = parseInt(prompt('ingresa tu edad:'));
